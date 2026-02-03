2890109b4feb5d79498840022e14e0108769a14d
SeratiVilankulu <seratimotla@gmail.com.com>
2026-02-03 16:04:39 +0200

    Adding search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+	let temperature = document.querySelector("#temperature");
+	temperature.innerHTML = Math.round(response.data.temperature.current);
+
+	let city = document.querySelector("#city");
+	city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+	let apiKey = "c42btd9a6f7of80d48aa81b1c6310a37";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+	axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSubmitForm(event) {
 	event.preventDefault();
 	let search = document.querySelector("#search-form-input");
-	let city = document.querySelector("#city");
-	city.innerHTML = search.value;
+
+	searchCity(search.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmitForm);
+
+searchCity("Pretoria");
