68d752a1982e6d2b99a41e162dea9ace2b4f3937
SeratiVilankulu <seratimotla@gmail.com.com>
2026-02-04 14:43:15 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 function updateWeather(response) {
 	let temperature = document.querySelector("#temperature");
 	temperature.innerHTML = Math.round(response.data.temperature.current);
-
 	let city = document.querySelector("#city");
+	let description = document.querySelector("#description");
+	let humidity = document.querySelector("#humidity");
+	let windSpeed = document.querySelector("#wind-speed");
+	let currentDate = document.querySelector("#date-time");
+	let date = new Date(response.data.time * 1000);
+
 	city.innerHTML = response.data.city;
+	currentDate.innerHTML = formatDate(date);
+	description.innerHTML = response.data.condition.description;
+	humidity.innerHTML = `${response.data.temperature.humidity}%`;
+	windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	let daysOfWeek = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+
+	let day = daysOfWeek[date.getDay()];
+
+	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
