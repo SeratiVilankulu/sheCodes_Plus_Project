804642d218ed5a4dfff73cebfe781f5bb47eb674
SeratiVilankulu <seratimotla@gmail.com.com>
2026-02-11 14:45:24 +0200

    removing console.logs

--- src/index.js
+++ src/index.js
@@ -75,7 +75,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-	console.log(response.data);
 	let forecastHtml = "";
 
 	response.data.daily.forEach(function (day, index) {
